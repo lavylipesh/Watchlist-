ec312a781f062cf0c3cef2f92d33c963136a6a1a
lavylipesh <lilibethlangat@yahoo.com>
2019-09-22 00:19:10 -0400

    processing results from movie api

--- app/requests.py
+++ app/requests.py
@@ -16,3 +16,19 @@ def get_movies(category):
             movie_results = process-results(movie_results_list)
 
             return movie_results
+
+def process_results(movie_list):
+    movie_results =[]
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_results.apppend(movie_object)
+
+            return movie_results
