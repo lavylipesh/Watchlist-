d208aca9bb5643e489a234192a26d58e03da1e45
lavylipesh <lilibethlangat@yahoo.com>
2019-09-22 00:24:31 -0400

    making API call

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from app import app
+from .requests import get_movies
 
 @app.route('/')
 def index():
-    title =''
-    return render_template('index.html',title=title)
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title ='Welcome to Watchlist'
+    return render_template('index.html',title=title,popular=popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
